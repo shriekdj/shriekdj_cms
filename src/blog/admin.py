1c6711bd57cfe1cc5f6f6dde939e134f13a4e5ad
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-08-10 15:54:37 +0530

    Deleted the blog app

--- src/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import NewsLetter, Post, PostMeta
-
-admin.site.register(NewsLetter)
-admin.site.register(Post)
-admin.site.register(PostMeta)
