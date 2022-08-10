928ee56cd93333988ba199456a672972807942cc
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-08-10 16:24:01 +0530

    because of dj migration error removed auto_now_add parameter from created_on

--- src/blog/models.py
+++ src/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
 	title = models.CharField(verbose_name='title', max_length=255, null=False)
 	content = models.TextField(verbose_name='content', null=False, blank=True)
-	created_on = models.DateTimeField(auto_now=True, auto_now_add=True, auto_created=True, null=False)
+	created_on = models.DateTimeField(auto_now=True, auto_created=True, null=False, blank=False)
 	published_on = models.DateTimeField(null=True)
 	updated_on = models.DateTimeField(null=True)
 
