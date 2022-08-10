c96730e3438345a346181c7e3755d4c42e6132fd
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-08-10 16:21:32 +0530

    Adding Dates to CMS

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(verbose_name='title', max_length=255, null=False)
+	content = models.TextField(verbose_name='content', null=False, blank=True)
+	created_on = models.DateTimeField(auto_now=True, auto_now_add=True, auto_created=True, null=False)
+	published_on = models.DateTimeField(null=True)
+	updated_on = models.DateTimeField(null=True)
+
+
