a61476acf21372e0815b8ccbfed8c9c5a8f22e7a
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-08-13 14:08:21 +0530

    Added Slug with default save method

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,4 @@
+from typing import Iterable, Optional
 from django.db import models
 
 # Create your models here.
@@ -7,5 +8,8 @@ class Post(models.Model):
 	created_on = models.DateTimeField(auto_now=True, auto_created=True, null=False, blank=False)
 	published_on = models.DateTimeField(null=True)
 	updated_on = models.DateTimeField(null=True)
+	slug = models.SlugField(verbose_name='slug', unique=True)
 
+	def save(self, force_insert: bool = ..., force_update: bool = ..., using: Optional[str] = ..., update_fields: Optional[Iterable[str]] = ...) -> None:
+		return super().save(force_insert, force_update, using, update_fields)
 
