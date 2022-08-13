4e8f78f13cd46ea7b02b7d7207415b2dee0d2ad8
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-08-13 16:15:31 +0530

    Modified admin

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,4 +1,5 @@
 import random
+import this
 from typing import Iterable, Optional
 from django.db import models
 from django.utils.text import slugify
@@ -10,13 +11,13 @@ def random_words():
 class Post(models.Model):
 	title = models.CharField(verbose_name='title', max_length=255, null=False)
 	content = models.TextField(verbose_name='content', null=False, blank=True)
-	created_on = models.DateTimeField(auto_now=True, auto_created=True, null=False, blank=False)
-	published_on = models.DateTimeField(null=True)
-	updated_on = models.DateTimeField(null=True)
-	slug = models.SlugField(verbose_name='slug', max_length=255, unique=True)
+	created_at = models.DateTimeField(verbose_name='created_at',auto_now=True, auto_created=True, null=False, blank=False)
+	published_at = models.DateTimeField(verbose_name='published_at',null=True)
+	updated_at = models.DateTimeField(verbose_name='updated_at',null=True)
+	slug = models.SlugField(verbose_name='slug', max_length=255, unique=True, default='')
 
 	def save(self, *args, **kwargs):
-		if not self.slug:
+		if self.slug == '':
 			self.slug = slugify(self.title + random_words())
 		super(Post, self).save(*args, **kwargs)
 
