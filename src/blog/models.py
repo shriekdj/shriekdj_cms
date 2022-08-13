ef653968e4d29f313044a4129da75dedfca76977
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-08-13 15:55:10 +0530

    Created Slug Engine

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,5 +1,10 @@
+import random
 from typing import Iterable, Optional
 from django.db import models
+from django.utils.text import slugify
+
+def random_words():
+    return ''.join(random.choice('abcdefghijklmnopqrstuvwxyz0123456789'.split('')) for _ in range(6))
 
 # Create your models here.
 class Post(models.Model):
@@ -8,8 +13,9 @@ class Post(models.Model):
 	created_on = models.DateTimeField(auto_now=True, auto_created=True, null=False, blank=False)
 	published_on = models.DateTimeField(null=True)
 	updated_on = models.DateTimeField(null=True)
-	slug = models.SlugField(verbose_name='slug', unique=True)
+	slug = models.SlugField(verbose_name='slug', max_length=255, unique=True)
 
-	def save(self, force_insert: bool = ..., force_update: bool = ..., using: Optional[str] = ..., update_fields: Optional[Iterable[str]] = ...) -> None:
-		return super().save(force_insert, force_update, using, update_fields)
-
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.title + random_words())
+		super(Post, self).save(*args, **kwargs)
